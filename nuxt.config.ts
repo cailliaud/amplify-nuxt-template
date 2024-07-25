15556a309223de39e5ed49c11f83403e8f29ff69
Alexandre CAILLIAUD <acailliaud@ippon.fr>
2024-07-25 13:25:14 +0200

    chore(spa): setup spa mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  hooks: {
+    'prerender:routes'({routes}){
+      routes.clear() // Do not generate any routes (except the defaults)
+    }
+  },
+  ssr: false,
+  router: {
+    options: {
+      hashMode: true // Avoid to configure redirect for pages on nginx or any hosting server
+    }
+  }
 })
